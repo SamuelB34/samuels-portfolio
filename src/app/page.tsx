08884379299abe35b2488b272b2eef36deda9140
SamuelBM1 <sbarragan@sales-hub.com>
2024-01-22 16:56:29 -0800

    Lottie fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ import { MyWork } from '@/app/_components/my-work/MyWork'
 import { MyCourses } from '@/app/_components/my-courses/MyCourses'
 import { Contact } from '@/app/_components/contact/Contact'
 import { Dots } from '@/app/_components/dots/Dots'
-import { useState } from 'react'
-import Lottie from 'lottie-react'
-
+import { useEffect, useRef, useState } from 'react'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
 import lottie from '@/shared/lottie/data.json'
 
 export default function Home() {
 	const [section, setSection] = useState(0)
+	const lottieRef = useRef<LottieRefCurrentProps | null>(null)
 
 	const scrollToElement = (view: string) => {
 		const element = document.getElementById(view)
@@ -28,9 +28,15 @@ export default function Home() {
 		let offset = event.currentTarget.offsetHeight - 16
 		const total = Math.floor(scroll / offset)
 		if (total !== section) {
-			setSection(Math.floor(scroll / offset))
+			const count = Math.floor(scroll / offset)
+			setSection(count)
+			console.log(count)
+			if (count > 0) {
+				// lottieRef.current?.playSegments([], true)
+			}
 		}
 	}
+
 	return (
 		<>
 			<Header />
@@ -43,7 +49,8 @@ export default function Home() {
 				<Lottie
 					animationData={lottie}
 					className={styles['background-lottie']}
-					loop={false}
+					lottieRef={lottieRef}
+					autoplay={false}
 				/>
 				<Presentation id={'presentation'} />
 				<AboutMe id={'about-me'} />
